db61d8390a35e8b2aa2b707a984f021849510583
carolbauer <99449590+Carolbauer@users.noreply.github.com>
2023-03-02 09:00:41 -0300

    Atividades Banco de dados

---
+++ 2023-1/Banco de dados/Revisão_FDB/revisao.sql
@@ -0,0 +1,71 @@
+create TABLE cidades (
+  codcid int AUTO_INCREMENT PRIMARY KEY,
+  nome varchar(20) NOT NULL,
+  uf varchar(2) NOT NULL
+  );
+
+  CREATE table filiais(
+  codfilial int AUTO_INCREMENT PRIMARY key,
+  nome varchar(100) not null,
+  endereco varchar(100) not null,
+  codcid int NOT null,
+  
+  CONSTRAINT codcid_cidade_fk
+  FOREIGN KEY (codcid)
+  REFERENCES Cidade(codcid)
+
+  on delete restrict
+  on update cascade
+  );
+  
+  create table empregados(
+  codemp varchar(10) PRIMARY key, 
+  nome varchar(100) not null,
+  endereco varchar(100) not null, 
+  ct varchar(20) NOT NULL,
+  rg varchar(20), 
+  cpf varchar(11) not null, 
+  salario int not null,
+  codfilial int not null,
+  codcid int not null,
+  
+  CONSTRAINT cidade_empregados_fk
+  FOREIGN KEY(codcid)
+  REFERENCES cidades(codcid),
+  
+  CONSTRAINT filial_empregados_fk
+  FOREIGN KEY(codfilial)
+  REFERENCES filiais(codfilial)
+);
+
+create table produtos
+(
+	cod_prod int auto_increment primary key,
+    descricao varchar(100) not null,
+    preco decimal(12,2) not null,
+    nome_categoria varchar(100) not null,
+    descricao_categoria varchar(100) not null
+);
+
+create table vendas
+(
+	cod_prod int not null,
+    codfilial  int not null,
+    constraint produtos_vendas_fk
+	foreign key(cod_prod) 
+	references produtos(cod_prod),
+    
+	constraint filiais_vendas_fk
+	foreign key(codfilial) 
+    references filiais(codfilial)
+    
+	on delete restrict
+	on update cascade
+);
+
+insert into produtos(descricao, preco, nome_categoria, descricao_categoria) VALUES (' Macbook Air 2020', 1200.00, 'Eletrônicos', 'tecnologia'),
+('Microondas', 350.00, 'Eletrodomésticos', 'cozinha'), ('Alexa', 700.00, 'Eletrônicos','tecnologia');
+
+Select max(preco)
+from produtos;
+
