781a829d60b17cb75ad9c16668a62db351aeb420
carolbauer <99449590+Carolbauer@users.noreply.github.com>
2023-03-10 18:35:05 -0300

    praticando laços de repetição

---
+++ 2023-1/Laboratório de programação/aula_2/src/ex2_3.java
@@ -0,0 +1,15 @@
+//Começando em 0, imprima os números seguintes, enquanto a soma dos números já
+//impressos for menor que 100.
+public class ex2_3 {
+    public static void main(String[] args){
+        int soma = 0;
+        int i = 0;
+
+        while (soma < 100){
+            System.out.println(i);
+                    soma +=i;
+            i++;
+        }
+
+    }
+}
---
+++ 2023-1/Laboratório de programação/aula_2/src/ex2_4.java
@@ -0,0 +1,14 @@
+public class ex2_4 {
+    public static void main(String[] args){
+        int numero = 9;
+        
+        for (int i = 0; i <=10; i++){
+            int tabuada = numero * i;
+            System.out.println(numero + "x" + i + "=" + tabuada);
+    
+        }
+        
+    }
+    
+    
+}
