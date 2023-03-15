60e00a404c48e566cf7181d78c891f8777dddc38
carolbauer <99449590+Carolbauer@users.noreply.github.com>
2023-03-15 20:46:52 -0300

    exercicio Data

--- 2023-1/Laboratório de programação/aula_2/src/ex5.java
+++ 2023-1/Laboratório de programação/aula_2/src/ex5.java
@@ -1,8 +1,5 @@
 
-public class ex5 {
-    /**
-     * @param args
-     */
+public  class ex5 {
     public static void main(String[] args){
         int dia = 15;
         int mes  = 03;
@@ -11,13 +8,18 @@ public class ex5 {
         if(ano < 1900 || ano >2999 || mes <1 || mes > 12 || dia < 1 || dia > 31){
             System.out.println("Data Inválida!");
         }else if ((dia <= 31) && (mes <=12 )){
-            if((dia >28)&& (mes == 2) || (dia == 31)&& (mes==4)|| (mes ==6)|| (mes == 9)|| (mes == 11));
-            System.out.println("Data inválida!");
+            if (((dia > 28) && (mes == 2)) || ((dia == 31) && ((mes == 4)
+            ||(mes == 6) || (mes == 9) || (mes == 11)))){
+                System.out.println("Data inválida!");
+            }
+            
         }else{
             System.out.println("Data Válida");
         }
         
+        
 
     }
+    
     
 }
