db2283382e50e9e2935f7bca51734c0c6afde7d3
carolbauer <99449590+Carolbauer@users.noreply.github.com>
2023-03-27 16:19:04 -0300

    exercícios orientação aobjetos

---
+++ 2023-1/Laboratório de programação/figurasGeometricas/src/Trapezio.java
@@ -0,0 +1,10 @@
+public class Trapezio {
+    double baseMaior;
+    double baseMenor;
+    double altura;
+
+    double calcularArea() {
+        double area = ((baseMaior + baseMenor) / 2) * altura;
+        return area;
+    }
+}
---
+++ 2023-1/Laboratório de programação/fracao/src/Programa.java
@@ -0,0 +1,13 @@
+public class Programa {
+    public static void main(String[] args) {
+        /*criar a fracao 5/2 e definir valores*/
+        Fracao f1 = new Fracao(3, 4);
+        /*criar a fracao 1/7 e definir valores*/
+        Fracao f2 = new Fracao(5, 6);
+        /*multiplica as fracoes*/
+        Fracao resultado = f1.multiplicar(f2);
+
+        /*mostra o valor do numerador e denominador, no caso a fracao e seu valor*/
+        System.out.println("Resultado: " + resultado.getNumerador() + "/" + resultado.getDenominador());
+    }
+}
