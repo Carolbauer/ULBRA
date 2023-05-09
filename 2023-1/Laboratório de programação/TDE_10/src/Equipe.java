11a7f11f59cb07ea7ab492e213f2fa41690eb831
carolbauer <99449590+Carolbauer@users.noreply.github.com>
2023-05-08 21:28:15 -0300

    Atualizando TDE

--- 2023-1/Laboratório de programação/TDE_10/src/Equipe.java
+++ 2023-1/Laboratório de programação/TDE_10/src/Equipe.java
@@ -3,6 +3,13 @@ public class Equipe {
     private int vitorias;
     private int derrotas;
     private int empates;
+    private int pontos;
+    private int kills;
+
+
+
+
+
 
     //construtor
     public Equipe(String nome, int vitorias, int derrotas, int empates){
@@ -10,6 +17,8 @@ public class Equipe {
         this.vitorias = 0 ;
         this.derrotas = 0 ;
         this.empates = 0 ;
+        this.pontos = 0;
+
     }
 
     public Equipe(String nome){
@@ -17,6 +26,7 @@ public class Equipe {
         this.vitorias = 0 ;
         this.derrotas = 0 ;
         this.empates = 0 ;
+        this.pontos = 0;
     }
 
     public String getNome() {
@@ -51,15 +61,53 @@ public class Equipe {
         this.empates = empates;
     }
 
-    public void registrarVitoria(){
+    public int getPontos() {
+        return pontos;
+    }
+
+    public void setPontos(int pontos) {
+        this.pontos = pontos;
+    }
+
+    public void registrarVitoria(int kills){
+
         this.vitorias++;
+        this.pontos += 3;
+        this.kills = this.kills + kills;
+
     }
 
-    public void registrarDerrota(){
+    public void registrarDerrota(int kills){
+
         this.derrotas++;
+        this.kills = this.kills + kills;
     }
 
-    public  void registrarEmpate(){
+    public  void registrarEmpate(int kills){
+
         this.empates++;
+        this.pontos++;
+        this.kills = this.kills + kills;
     }
+
+    public void exibirInformacoes(){
+        System.out.println("Derrotas " + this.derrotas);
+        System.out.println("Vitórias" + this.vitorias);
+        System.out.println("Empates" + this.empates);
+        System.out.println("Pontos" + this.pontos);
+        System.out.println("Aproveitamento" + this.calcularVitoria());
+        System.out.println("Número de Kills:" + this.kills);
+    }
+
+    public double calcularVitoria(){
+        int totalJogos = this.derrotas + this.vitorias + this.empates;
+        int totalPontosPossiveis = totalJogos * 3;
+        double aproveitamento = 0;
+        if(totalPontosPossiveis > 0){
+            aproveitamento = (double) pontos / totalPontosPossiveis * 100;
+        }
+        return aproveitamento;
+    }
+
+
 }
--- 2023-1/Laboratório de programação/TDE_10/src/GerenciamentoCampeonato.java
+++ 2023-1/Laboratório de programação/TDE_10/src/GerenciamentoCampeonato.java
@@ -3,18 +3,20 @@ public class GerenciamentoCampeonato {
         Equipe equipe1 = new Equipe("ADS");
         Equipe equipe2 = new Equipe("ENF");
 
-        equipe1.registrarVitoria();
-        equipe2.registrarVitoria();
-        equipe2.registrarDerrota();
-        equipe1.registrarEmpate();
-        equipe1.registrarVitoria();
-        equipe2.registrarDerrota();
-        equipe1.registrarDerrota();
-        equipe1.registrarEmpate();
-
+        equipe1.registrarVitoria(0);
+        equipe2.registrarVitoria(0);
+        equipe2.registrarDerrota(3);
+        equipe1.registrarEmpate(0);
+        equipe1.registrarVitoria(0);
+        equipe2.registrarDerrota(3);
+        equipe1.registrarDerrota(3);
+        equipe1.registrarEmpate(0);
+        equipe1.exibirInformacoes();
+        equipe2.exibirInformacoes();
+        /*
         System.out.println("Nome equipe:"+ " " + "  " + "V" + " "+ "D" + " " + "E");
         System.out.println("Equipe 1 - " + equipe1.getNome()+ " " + equipe1.getVitorias() + " " + equipe1.getDerrotas() + " " + equipe1.getEmpates());
         System.out.println("Equipe 2 - " + equipe2.getNome()+ " " + equipe2.getVitorias() + " " + equipe2.getDerrotas() + " " + equipe2.getEmpates());
-
+         */
     }
 }
