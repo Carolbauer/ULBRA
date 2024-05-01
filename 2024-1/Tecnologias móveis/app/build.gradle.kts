4729411b7167637403a60e0d076a3e0680d2836f
carolbauer <99449590+Carolbauer@users.noreply.github.com>
2024-05-01 15:27:17 -0300

    continuação apk

--- 2024-1/Tecnologias móveis/app/build.gradle.kts
+++ 2024-1/Tecnologias móveis/app/build.gradle.kts
@@ -41,6 +41,9 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
 
+    //Glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
--- 2024-1/Tecnologias móveis/app/src/main/java/br/com/ulbra/tde09/ProductAdapter.kt
+++ 2024-1/Tecnologias móveis/app/src/main/java/br/com/ulbra/tde09/ProductAdapter.kt
@@ -1,14 +1,19 @@
 package br.com.ulbra.tde09
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 class ProductAdapter (private val items: MutableList<Product>) : RecyclerView.Adapter<ProductAdapter.ViewHolder>() {
+
+    private lateinit var context: Context
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductAdapter.ViewHolder {
+        context = parent.context
 
         val view = LayoutInflater.from(parent.context).inflate(R.layout.product_item, parent, false)
         return ViewHolder(view)
@@ -16,6 +21,9 @@ class ProductAdapter (private val items: MutableList<Product>) : RecyclerView.Ad
 
     override fun onBindViewHolder(holder: ProductAdapter.ViewHolder, position: Int) {
         holder.priceProduct.text = items[position].price
+        holder.nameProduct.text = items[position].name
+
+        Glide.with(context).load(items[position].urlImage).into(holder.imageProduct)
     }
 
     override fun getItemCount(): Int {
