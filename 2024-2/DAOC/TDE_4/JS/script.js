3c2577fb78c8a0f7acb9a6d367bfd4a4db35ccf9
carolbauer <99449590+Carolbauer@users.noreply.github.com>
2024-08-29 21:15:09 -0300

    html, css, style

---
+++ 2024-2/DAOC/TDE_4/JS/script.js
@@ -0,0 +1,14 @@
+const menuToggle = document.getElementById('menu-toggle');
+const menu = document.querySelector('#menu ul');
+
+menuToggle.addEventListener('click', () => {
+  menu.classList.toggle('disable');
+});
+
+function navigate(page) {
+ document.querySelectorAll('main > section').forEach((section) => {
+    section.classList.add('disable');
+    });
+    
+    document.getElementById(page).classList.remove('disable');
+ };
