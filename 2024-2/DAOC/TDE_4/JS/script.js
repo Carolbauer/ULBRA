9d1565b6b772c5daae6c9dc7a9aae7c60bcfff47
carolbauer <99449590+Carolbauer@users.noreply.github.com>
2024-09-02 20:49:38 -0300

    js

--- 2024-2/DAOC/TDE_4/JS/script.js
+++ 2024-2/DAOC/TDE_4/JS/script.js
@@ -5,10 +5,46 @@ menuToggle.addEventListener('click', () => {
   menu.classList.toggle('disable');
 });
 
-function navigate(page) {
- document.querySelectorAll('main > section').forEach((section) => {
+function navigate(page){
+  const sections = document.querySelectorAll('main > section');
+
+  sections.forEach(section => {
     section.classList.add('disable');
-    });
-    
-    document.getElementById(page).classList.remove('disable');
- };
+  })
+
+  document.getElementById(page).classList.remove('disable')
+}
+
+function submitForm(){
+  const contact = {
+    name: document.getElementById("inputName").value,
+    email: document.getElementById("inputEmail").value,
+    phone: document.getElementById("inputPhone").value,
+    message: document.getElementById("inputMessage").value
+  };
+
+  getContact(contact)
+}
+
+function getContact(contact){
+  const table = `
+    <table> 
+      <thead> 
+        <tr>
+          <th>Nome</th>
+          <th>Email</th>
+          <th>Telefone</th>
+        </tr>
+      </thead>
+      <tbody> 
+        <tr>
+          <td>${contact.name}</td>
+          <td>${contact.email}</td>
+          <td>${contact.phone}</td>
+        </tr>
+      </tbody>
+    </table>
+  `
+
+  document.getElementById("tableContact").innerHTML = table;
+}
