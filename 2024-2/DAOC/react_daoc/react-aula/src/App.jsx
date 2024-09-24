88ce18b54da997c0c0dfb49c5edfc44ffe279a8d
carolbauer <99449590+Carolbauer@users.noreply.github.com>
2024-09-23 21:55:09 -0300

    react

---
+++ 2024-2/DAOC/react_daoc/react-aula/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Gallery from './components/Gallery'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <Gallery />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ 2024-2/DAOC/react_daoc/react-aula/src/components/Profile.jsx
@@ -0,0 +1,11 @@
+const Profile = () => {
+    return <img src="https://i.imgur.com/MK3eW3As.jpg" />;
+
+    <div>
+        <h1>Minha Galeria</h1>
+        
+    </div>
+    
+    }
+
+    export default Profile;
